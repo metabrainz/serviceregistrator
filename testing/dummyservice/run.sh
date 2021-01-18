c9f229c23d0cc90127e3cd893a1b60dc7c0cbcfd
Laurent Monin <github@norz.org>
2021-01-18 19:46:38 +0100

    Complexify test

--- testing/dummyservice/run.sh
+++ testing/dummyservice/run.sh
@@ -7,20 +7,22 @@ function runservice() {
 	NAME="dummyservice${EXTPORT}"
 	docker rm -f "$NAME"
 	docker run -d \
-		--hostname "$HOSTNAME" \
-		--env "SERVICE_NAME=dummyservicenoportfromenv" \
-		--env "SERVICE_TAGS=noporttag" \
-		--label "SERVICE_NAME=dummyservicenoportfromlabel" \
-		--label "SERVICE_CHECK_INTERVAL=25s" \
+		--env "SERVICE_CHECK_TIMEOUT=15s" \
 		--env "SERVICE_${INTPORT}_CHECK_TCP=true" \
 		--env "SERVICE_${INTPORT}_CHECK_TIMEOUT=10s" \
 		--env "SERVICE_${INTPORT}_NAME=dummyservice" \
+		--env "SERVICE_NAME=dummyservicenoportfromenv" \
+		--env "SERVICE_TAGS=noporttag" \
+		--hostname "$HOSTNAME" \
+		--label "SERVICE_180_CHECK_TCP=false" \
 		--label "SERVICE_180_NAME=dummyservice180" \
+		--label "SERVICE_CHECK_INTERVAL=25s" \
 		--label "SERVICE_${INTPORT}_TAGS=prod,dummytag" \
+		--label "SERVICE_NAME=dummyservicenoportfromlabel" \
 		--name "$NAME" \
-		--publish "${EXTPORT}:${INTPORT}" \
-		--publish "2${EXTPORT}:${INTPORT}" \
 		--publish "1${EXTPORT}:1${INTPORT}/udp" \
+		--publish "2${EXTPORT}:${INTPORT}" \
+		--publish "${EXTPORT}:${INTPORT}" \
 		dummyservice
 }
 
