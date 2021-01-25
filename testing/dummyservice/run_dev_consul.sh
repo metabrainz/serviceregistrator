a046f792de70870203e0e843fe2989f3c3caffcd
Laurent Monin <github@norz.org>
2021-01-25 11:05:33 +0100

    Let user pass consul image version on command-line

--- testing/dummyservice/run_dev_consul.sh
+++ testing/dummyservice/run_dev_consul.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ ! -z "$1"]; then
+	CONSUL_IMAGE_VERSION=$1
+else
+	CONSUL_IMAGE_VERSION=latest
+fi
+
+IMAGE=library/consul:$CONSUL_IMAGE_VERSION
+
 docker rm -f dev-consul
 
 #note: enable_script_checks for testing only, unsecure
-docker run -d --name=dev-consul -e CONSUL_BIND_INTERFACE=lo -e CONSUL_LOCAL_CONFIG='{"enable_script_checks": true}' -v /var/run/docker.sock:/var/run/docker.sock --net=host consul
+docker run -d --name=dev-consul -e CONSUL_BIND_INTERFACE=lo -e CONSUL_LOCAL_CONFIG='{"enable_script_checks": true}' -v /var/run/docker.sock:/var/run/docker.sock --net=host $IMAGE
