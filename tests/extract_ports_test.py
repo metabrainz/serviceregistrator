33e9c6b3666612da317dc707adfaa44583cd8b1c
Laurent Monin <github@norz.org>
2021-01-26 12:09:34 +0100

    Improve extract_ports() tests

--- tests/extract_ports_test.py
+++ tests/extract_ports_test.py
@@ -2,30 +2,28 @@ import unittest
 from serviceregistrator.registrator import ServiceRegistrator, Ports
 
 
-class DummyContainer:
-    attrs = {
-        'NetworkSettings': {
-            'Ports': {
-                '180/udp': [{'HostIp': '127.0.0.1', 'HostPort': '18082'}],
-                '80/tcp': [{'HostIp': '0.0.0.0', 'HostPort': '28082'},
-                           {'HostIp': '0.0.0.0', 'HostPort': '8082'}]}
-        },
-        'HostConfig': {
-            "NetworkMode": "default",
-        },
-        "Config": {
-            "Hostname": "ifrit5",
-            "ExposedPorts": {
-                "180/udp": {},
-                "80/tcp": {}
-            },
-        }
-    }
-
-
-class TestExtractPorts(unittest.TestCase):
-
+class TestExtractPortsDefault(unittest.TestCase):
     def setUp(self):
+        class DummyContainer:
+            attrs = {
+                'NetworkSettings': {
+                    'Ports': {
+                        '180/udp': [{'HostIp': '127.0.0.1', 'HostPort': '18082'}],
+                        '80/tcp': [{'HostIp': '0.0.0.0', 'HostPort': '28082'},
+                                   {'HostIp': '0.0.0.0', 'HostPort': '8082'}]}
+                },
+                'HostConfig': {
+                    "NetworkMode": "default",
+                },
+                "Config": {
+                    "Hostname": "my_hostname",
+                    "ExposedPorts": {
+                        "180/udp": {},
+                        "80/tcp": {}
+                    },
+                }
+            }
+
         self.container = DummyContainer()
 
     def test_extract_ports(self):
@@ -35,9 +33,53 @@ class TestExtractPorts(unittest.TestCase):
             Ports(internal=80, external=28082, protocol='tcp', ip='0.0.0.0'),
             Ports(internal=80, external=8082, protocol='tcp', ip='0.0.0.0')]))
 
-    def test_extract_ports_hostmode(self):
-        self.container.attrs['HostConfig']['NetworkMode'] = 'host'
+    def test_extract_ports_no_port(self):
+        self.container.attrs['NetworkSettings']['Ports'] = {}
+        ports = ServiceRegistrator.extract_ports(self.container)
+        self.assertEqual(ports, [])
+
+
+class TestExtractPortsBridge(TestExtractPortsDefault):
+
+    def setUp(self):
+        super().setUp()
+        self.container.attrs['HostConfig']['NetworkMode'] = 'bridge'
+        print(self.container.attrs)
+
+
+class TestExtractPortsHost(unittest.TestCase):
+
+    def setUp(self):
+        class DummyContainer:
+            attrs = {
+                'NetworkSettings': {
+                    'Networks': {
+                        'host': {
+                            'IPAddress': '',
+                        }
+                    }
+                },
+                'HostConfig': {
+                    "NetworkMode": "host",
+                },
+                "Config": {
+                    "Hostname": "my_hostname",
+                    "ExposedPorts": {
+                        "180/udp": {},
+                        "80/tcp": {}
+                    },
+                }
+            }
+
+        self.container = DummyContainer()
+
+    def test_extract_ports(self):
         ports = ServiceRegistrator.extract_ports(self.container)
         self.assertEqual(set(ports), set([
             Ports(internal=80, external=80, protocol='tcp', ip='0.0.0.0'),
             Ports(internal=180, external=180, protocol='udp', ip='0.0.0.0'), ]))
+
+    def test_extract_ports_no_port(self):
+        self.container.attrs['Config']['ExposedPorts'] = {}
+        ports = ServiceRegistrator.extract_ports(self.container)
+        self.assertEqual(ports, [])
